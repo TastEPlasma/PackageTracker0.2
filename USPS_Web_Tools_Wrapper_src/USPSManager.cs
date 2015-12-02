0179aa92b034809c21c4e9400d010d2d6a98f477
ER Webster <TastEPlasma@gmail.com>
2015-12-02 17:40:36 -0600

    Fixed incorrect reference to USPS web service

--- USPS_Web_Tools_Wrapper_src/USPSManager.cs
+++ USPS_Web_Tools_Wrapper_src/USPSManager.cs
@@ -19,10 +19,20 @@ namespace MAX.USPS
         private const string ProductionUrl = "http://production.shippingapis.com/ShippingAPI.dll";
         private const string TestingUrl = "http://testing.shippingapis.com/ShippingAPITest.dll";
         private WebClient web;
-        private string _userid;
+        private string _userid = "857STUDE5322";
+
+        private bool _TestMode;
         #endregion
 
         #region Constructors
+        /// <summary>
+        /// Constructor that requires no arguments, uses default ID
+        /// </summary>
+        public USPSManager()
+        {
+            web = new WebClient();
+            _TestMode = false;
+        }
 
         /// <summary>
         /// Creates a new USPS Manager instance
@@ -50,138 +60,16 @@ namespace MAX.USPS
         #endregion
 
         #region Properties
-        private bool _TestMode;
-        /// <summary>
-        /// Determines if the Calls to the USPS server is made to the Test or Production server.
-        /// </summary>
+        public string setUserID 
+        {
+            get { return _userid; }
+            set { _userid = value; }
+        }
+
         public bool TestMode
         {
             get { return _TestMode; }
             set { _TestMode = value; }
-        }
-
-        #endregion
-
-        #region Address Methods
-        /// <summary>
-        /// Validate a single address
-        /// </summary>
-        /// <param name="address">Address object to be validated</param>
-        /// <returns>Validated Address</returns>
-        public Address ValidateAddress(Address address)
-        {
-            try
-            {
-                string validateUrl = "?API=Verify&XML=<AddressValidateRequest USERID=\"{0}\"><Address ID=\"{1}\"><Address1>{2}</Address1><Address2>{3}</Address2><City>{4}</City><State>{5}</State><Zip5>{6}</Zip5><Zip4>{7}</Zip4></Address></AddressValidateRequest>";
-                string url = GetURL() + validateUrl;
-                url = String.Format(url, _userid, address.ID.ToString(), address.Address1, address.Address2, address.City, address.State, address.Zip, address.ZipPlus4);
-                string addressxml = web.DownloadString(url);
-                if (addressxml.Contains("<Error>"))
-                {
-                    int idx1 = addressxml.IndexOf("<Description>") + 13;
-                    int idx2 = addressxml.IndexOf("</Description>");
-                    int l = addressxml.Length;
-                    string errDesc = addressxml.Substring(idx1, idx2 - idx1);
-                    throw new USPSManagerException(errDesc);
-                }
-                
-                return Address.FromXml(addressxml);
-            }
-            catch(WebException ex)
-            {
-                throw new USPSManagerException(ex);
-            }
-        }
-        /// <summary>
-        /// Get the zip code by providing an Address object with a city and state
-        /// </summary>
-        /// <param name="city">City</param>
-        /// <param name="state">State</param>
-        public Address GetZipcode(string city, string state)
-        {
-            Address a = new Address();
-            a.City = city;
-            a.State = state;
-            return GetZipcode(a);
-        }
-
-        /// <summary>
-        /// Get the zip code by providing an Address object with a city and state
-        /// </summary>
-        /// <param name="address">Address Object</param>
-        /// <returns>Address Object</returns>
-        public Address GetZipcode(Address address)
-        {
-            try
-            {
-                //The address must contain a city and state
-                if (address.City == null || address.City.Length < 1 || address.State == null || address.State.Length < 1)
-                    throw new USPSManagerException("You must supply a city and state for a zipcode lookup request.");
-
-                string zipcodeurl = "?API=ZipCodeLookup&XML=<ZipCodeLookupRequest USERID=\"{0}\"><Address ID=\"{1}\"><Address1>{2}</Address1><Address2>{3}</Address2><City>{4}</City><State>{5}</State></Address></ZipCodeLookupRequest>";
-                string url = GetURL() + zipcodeurl;
-                url = String.Format(url, _userid, address.ID.ToString(), address.Address1, address.Address2, address.City, address.State, address.Zip, address.ZipPlus4);
-                string addressxml = web.DownloadString(url);
-                if (addressxml.Contains("<Error>"))
-                {
-                    int idx1 = addressxml.IndexOf("<Description>") + 13;
-                    int idx2 = addressxml.IndexOf("</Description>");
-                    int l = addressxml.Length;
-                    string errDesc = addressxml.Substring(idx1, idx2 - idx1);
-                    throw new USPSManagerException(errDesc);
-                }
-
-                return Address.FromXml(addressxml);
-            }
-            catch (WebException ex)
-            {
-                throw new USPSManagerException(ex);
-            }
-        }
-
-        /// <summary>
-        /// Get the city and state by proving the zip code.
-        /// </summary>
-        /// <param name="zipcode">Zipcode</param>
-        public Address GetCityState(string zipcode)
-        {
-            Address a = new Address();
-            a.Zip = zipcode;
-            return GetCityState(a);
-        }
-
-        /// <summary>
-        /// Get the city and state by proving the zip code.
-        /// </summary>
-        /// <param name="address">Address object</param>
-        /// <returns>Address Object</returns>
-        public Address GetCityState(Address address)
-        {
-            try
-            {
-                //The address must contain a city and state
-                if (address.Zip == null || address.Zip.Length < 1)
-                    throw new USPSManagerException("You must supply a zipcode for a city/state lookup request.");
-                
-                string citystateurl = "?API=CityStateLookup&XML=<CityStateLookupRequest USERID=\"{0}\"><ZipCode ID= \"{1}\"><Zip5>{2}</Zip5></ZipCode></CityStateLookupRequest>";
-                string url = GetURL() + citystateurl;
-                url = String.Format(url, _userid, address.ID.ToString(), address.Zip);
-                string addressxml = web.DownloadString(url);
-                if (addressxml.Contains("<Error>"))
-                {
-                    int idx1 = addressxml.IndexOf("<Description>") + 13;
-                    int idx2 = addressxml.IndexOf("</Description>");
-                    int l = addressxml.Length;
-                    string errDesc = addressxml.Substring(idx1, idx2 - idx1);
-                    throw new USPSManagerException(errDesc);
-                }
-
-                return Address.FromXml(addressxml);
-            }
-            catch (WebException ex)
-            {
-                throw new USPSManagerException(ex);
-            }
         }
 
         #endregion
@@ -211,102 +99,9 @@ namespace MAX.USPS
                 throw new USPSManagerException(ex);
             }
         }
-        #endregion
-
-        #region Label Methods
-        /// <summary>
-        /// Fills a package's ShippingLabel with a Byte{} containing the Image for the label
-        /// </summary>
-        /// <param name="package">Package with From and To addresses provided</param>
-        /// <returns>The same package with the ShippingLabel</returns>
-        public Package GetDeliveryConfirmationLabel(Package package)
+        public void ResetResetCredentialsToDefaults()
         {
-            string labeldate = package.ShipDate.ToShortDateString();
-            if (package.ShipDate.ToShortDateString() == DateTime.Now.ToShortDateString())
-                labeldate = "";
-            string url = "?API=DeliveryConfirmationV3&XML=<DeliveryConfirmationV3.0Request USERID=\"{0}\"><Option>{1}</Option><ImageParameters></ImageParameters><FromName>{2}</FromName><FromFirm>{3}</FromFirm><FromAddress1>{4}</FromAddress1><FromAddress2>{5}</FromAddress2><FromCity>{6}</FromCity><FromState>{7}</FromState><FromZip5>{8}</FromZip5><FromZip4>{9}</FromZip4><ToName>{10}</ToName><ToFirm>{11}</ToFirm><ToAddress1>{12}</ToAddress1><ToAddress2>{13}</ToAddress2><ToCity>{14}</ToCity><ToState>{15}</ToState><ToZip5>{16}</ToZip5><ToZip4>{17}</ToZip4><WeightInOunces>{18}</WeightInOunces><ServiceType>{19}</ServiceType><POZipCode>{20}</POZipCode><ImageType>{21}</ImageType><LabelDate>{22}</LabelDate><CustomerRefNo>{23}</CustomerRefNo><AddressServiceRequested>{24}</AddressServiceRequested><SenderName>{25}</SenderName><SenderEMail>{26}</SenderEMail><RecipientName>{27}</RecipientName><RecipientEMail>{28}</RecipientEMail></DeliveryConfirmationV3.0Request>";
-            url = GetURL() + url;
-            //url = String.Format(url,this._userid, (int)package.LabelType, package.FromAddress.Contact, package.FromAddress.FirmName, package.FromAddress.Address1, package.FromAddress.Address2, package.FromAddress.City, package.FromAddress.State, package.FromAddress.Zip, package.FromAddress.ZipPlus4, package.ToAddress.Contact, package.ToAddress.FirmName, package.ToAddress.Address1, package.ToAddress.Address2, package.ToAddress.City, package.ToAddress.State, package.ToAddress.Zip, package.ToAddress.ZipPlus4, package.WeightInOunces.ToString(), package.ServiceType.ToString().Replace("_", " "), package.OriginZipcode, package.LabelImageType.ToString(), labeldate, package.ReferenceNumber, package.AddressServiceRequested.ToString(),  package.FromAddress.Contact, package.FromAddress.ContactEmail, package.ToAddress.Contact, package.ToAddress.ContactEmail);
-            url = String.Format(url, this._userid, (int)package.LabelType, package.FromAddress.Contact, package.FromAddress.FirmName, package.FromAddress.Address1, package.FromAddress.Address2, package.FromAddress.City, package.FromAddress.State, package.FromAddress.Zip, package.FromAddress.ZipPlus4, package.ToAddress.Contact, package.ToAddress.FirmName, package.ToAddress.Address1, package.ToAddress.Address2, package.ToAddress.City, package.ToAddress.State, package.ToAddress.Zip, package.ToAddress.ZipPlus4, package.WeightInOunces.ToString(), package.ServiceType.ToString().Replace("_", " "), package.OriginZipcode, package.LabelImageType.ToString(), labeldate, package.ReferenceNumber, package.AddressServiceRequested.ToString(), "", "", "", "");
-            string xml = web.DownloadString(url);
-            if (xml.Contains("<Error>"))
-            {
-                int idx1 = xml.IndexOf("<Description>") + 13;
-                int idx2 = xml.IndexOf("</Description>");
-                int l = xml.Length;
-                string errDesc = xml.Substring(idx1, idx2 - idx1);
-                throw new USPSManagerException(errDesc);
-            }
-            int i1 = xml.IndexOf("<DeliveryConfirmationLabel>") + 27;
-            int i2 = xml.IndexOf("</DeliveryConfirmationLabel>");
-            package.ShippingLabel = StringToUTF8ByteArray(xml.Substring(i1, i2 - i1));
-            return package;
-        }
-
-        /// <summary>
-        /// Fills a package's ShippingLabel with a Byte{} containing the Image for the label
-        /// </summary>
-        /// <param name="package">Package with From and To addresses provided</param>
-        /// <returns>The same package with the ShippingLabel</returns>
-        public Package GetSignatureConfirmationLabel(Package package)
-        {
-            string url = "?API=SignatureConfirmationV3&XML=<SignatureConfirmationV3.0Request USERID=\"{0}\"><Option>{1}</Option><ImageParameters></ImageParameters><FromName>{2}</FromName><FromFirm>{3}</FromFirm><FromAddress1>{4}</FromAddress1><FromAddress2>{5}</FromAddress2><FromCity>{6}</FromCity><FromState>{7}</FromState><FromZip5>{8}</FromZip5><FromZip4>{9}</FromZip4><ToName>{10}</ToName><ToFirm>{11}</ToFirm><ToAddress1>{12}</ToAddress1><ToAddress2>{13}</ToAddress2><ToCity>{14}</ToCity><ToState>{15}</ToState><ToZip5>{16}</ToZip5><ToZip4>{17}</ToZip4><WeightInOunces>{18}</WeightInOunces><ServiceType>{19}</ServiceType><POZipCode>{20}</POZipCode><ImageType>{21}</ImageType><LabelDate>{22}</LabelDate><CustomerRefNo>{23}</CustomerRefNo><AddressServiceRequested>{24}</AddressServiceRequested></SignatureConfirmationV3.0Request>";
-            url = GetURL() + url;
-            url = String.Format(url, this._userid, (int)package.LabelType, package.FromAddress.Contact, package.FromAddress.FirmName, package.FromAddress.Address1, package.FromAddress.Address2, package.FromAddress.City, package.FromAddress.State, package.FromAddress.Zip, package.FromAddress.ZipPlus4, package.ToAddress.Contact, package.ToAddress.FirmName, package.ToAddress.Address1, package.ToAddress.Address2, package.ToAddress.City, package.ToAddress.State, package.ToAddress.Zip, package.ToAddress.ZipPlus4, package.WeightInOunces.ToString(), package.ServiceType.ToString().Replace("_", " "), package.OriginZipcode, package.LabelImageType.ToString(), package.ShipDate.ToShortDateString(), package.ReferenceNumber, package.AddressServiceRequested.ToString(), package.FromAddress.Contact, package.FromAddress.ContactEmail, package.ToAddress.Contact, package.ToAddress.ContactEmail);
-            string xml = web.DownloadString(url);
-            if (xml.Contains("<Error>"))
-            {
-                int idx1 = xml.IndexOf("<Description>") + 13;
-                int idx2 = xml.IndexOf("</Description>");
-                int l = xml.Length;
-                string errDesc = xml.Substring(idx1, idx2 - idx1);
-                throw new USPSManagerException(errDesc);
-            }
-            int i1 = xml.IndexOf("<SignatureConfirmationLabel>") + 28;
-            int i2 = xml.IndexOf("</DeliveryConfirmationLabel>");
-            package.ShippingLabel = StringToUTF8ByteArray(xml.Substring(i1, i2 - i1));
-            return package;
-        }
-
-     
-        #endregion
-
-        #region Rates
-
-        public RateResponse GetRate(Package package)
-        {
-            try
-            {
-                string url = "?API=RateV2&XML=<RateV2Request USERID=\"{0}\"><Package ID=\"0\"><Service>{1}</Service><ZipOrigination>{2}</ZipOrigination><ZipDestination>{3}</ZipDestination><Pounds>{4}</Pounds><Ounces>{5}</Ounces><Container>{6}</Container><Size>{7}</Size></Package></RateV2Request>";
-
-                int lb = package.WeightInOunces / 16;
-                int oz = package.WeightInOunces % 16;
-                string container = package.PackageType.ToString().Replace("_", " ");
-                if (container == "None")
-                    url = url.Replace("<Container>{6}</Container>", "");
-                string fromZip = package.FromAddress.Zip;
-                if (package.OriginZipcode != null && package.OriginZipcode.Length > 0)
-                    fromZip = package.OriginZipcode;
-
-                
-                url = GetURL() + url;
-                url = String.Format(url, _userid, package.ServiceType.ToString(), fromZip, package.ToAddress.Zip, lb.ToString(), oz.ToString(), container, package.PackageSize.ToString().Replace("_", " "));
-                string xml = web.DownloadString(url);
-                if (xml.Contains("<Error>"))
-                {
-                    int idx1 = xml.IndexOf("<Description>") + 13;
-                    int idx2 = xml.IndexOf("</Description>");
-                    int l = xml.Length;
-                    string errDesc = xml.Substring(idx1, idx2 - idx1);
-                    throw new USPSManagerException(errDesc);
-                }
-
-                return RateResponse.FromXml(xml);
-            }
-            catch (WebException ex)
-            {
-                throw new USPSManagerException(ex);
-            }
+            _userid = "857STUDE5322";
         }
         #endregion
 
